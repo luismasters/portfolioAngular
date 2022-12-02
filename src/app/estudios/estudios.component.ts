78a15df8ddc519b723ad16bd1312625d594badf4
luismasters <luismasters360@gmail.com>
2022-12-02 20:48:57 -0300

    estudios

--- src/app/estudios/estudios.component.ts
+++ src/app/estudios/estudios.component.ts
@@ -41,9 +41,10 @@ export class EstudiosComponent implements OnInit {
   }
 
 
-  estudios:any[]=[
-{instituto:"Universidad de Carabobo", logo:"../assets/img/UC_logo.png",carrera:"Contaduria Publica"},
-{instituto:"UTN FRGP", logo:"../assets/img/logo-utn.png",carrera:"Tecnicatura Universitaria en Programacion"},
+  estudios: any[] = [{ instituto: "UTN FRGP.", logo: "../assets/img/logo-utn.png", carrera: "Tecnicatura Universitaria en Programacion.", Estado: "Cursando Actualmente 3er Semenstre de Programacion." },
+  { instituto: "Universidad Santiago Mariño.", logo: "../assets/img/logo-PSM PNG.png", carrera: "ingenieria Electronica.", Estado: "4to semestre del ciclo basico, no culminada." },
+  { instituto: "Universidad de Carabobo.", logo: "../assets/img/UC_logo.png", carrera: "Contaduria Publica.", Estado: "7mo semestre, no culminada." },
+  { instituto: "Argentina Programa.", logo: "../assets/img/APLogo-20-20.png", carrera: "Desarrollador Web Full Stack.", Estado: "Cursando." },
 
 
 
